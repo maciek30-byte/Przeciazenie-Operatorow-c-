b2564fe9472fc652ec9f7cc199c66f367d4bed73
maciej.opozda <maciej.opozda@PK1-16>
2022-03-26 18:52:05 +0100

    Dodaj pliki projektów.

---
+++ ConsoleApp1/Fraction.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Fraction
+    {
+       readonly long numerator;
+       readonly long denominator;
+
+        public Fraction(long first, long second)
+        {
+            if(numerator <= 0 || denominator<=0)
+            {
+              throw new ArgumentException("exception");
+            }
+            
+            numerator = first;
+            denominator = second;
+        }
+
+        public long CreateFraction()
+        {
+            return numerator/denominator;
+        }
+
+        public override string ToString()
+        {
+            return "${numerator}/{denominator}";
+        }
+
+        // implement add fraction method without przeciazenie operatora 
+
+        public static Fraction operator /(Fraction a, Fraction b)
+        {
+            return new Fraction(a.numerator * b.denominator, a.denominator * b.numerator);
+        }
+
+    
+
+       
+    }
+}
